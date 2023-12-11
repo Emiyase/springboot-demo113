bacd5f341a3acc81141ab2bfb632941b9447944f
YY233 <3152322562@qq.com>
2023-12-11 15:31:44 +0800

    第一次

---
+++ src/main/java/com/example/springbootdemo114/SpringbootDemo114Application.java
@@ -0,0 +1,16 @@
+package com.example.springbootdemo114;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemo114Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringbootDemo114Application.class, args);
+
+    }
+
+}
+
---
+++ src/test/java/com/example/springbootdemo114/SpringbootDemo114ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemo114;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemo114ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
